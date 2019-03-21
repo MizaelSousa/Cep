bab1feba7acf15cd13f959cd2f82586d18abdc1c
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-03-21 19:36:23 -0400

    primeiro commit do projeto

---
+++ Cep/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Cep.Model;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace Cep
+{
+    public partial class MainPage : ContentPage
+    {
+        async void Handle_Completed(object sender, System.EventArgs e)
+        {
+            var cliente = new HttpClient();
+            string cep = txtCEP.Text;
+            var json = await cliente.GetStringAsync($"http://viacep.com.br/ws/{cep}/json/");
+            var dados = JsonConvert.DeserializeObject<Endereco>(json);
+
+            lblLogradouro.Text  =  dados.logradouro;
+            lblComplemento.Text =  dados.logradouro;
+            lblBairro.Text      =  dados.logradouro;
+            lblLocalidade.Text  =  dados.logradouro;
+            lblUf.Text          =  dados.logradouro;
+            lblUnidade.Text     =  dados.logradouro;
+            lblIBGE.Text        =  dados.logradouro;
+            lblGIA.Text         =  dados.logradouro;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Cep/Model/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Cep.Model
+{
+    public class Endereco
+    {
+        public string cep { set; get; }
+        public string logradouro { set; get; }
+        public string complemento { set; get; }
+        public string bairro { set; get; }
+        public string localidade { set; get; }
+        public string uf { set; get; }
+        public string unidade { set; get; }
+        public string ibge { set; get; }
+        public string gia { set; get; }
+
+public Endereco()
+        {
+        }
+    }
+}
